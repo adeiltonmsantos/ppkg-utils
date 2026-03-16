c5aa79f5aed25f01320a13ae833dd0afafc9fb04
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-03-16 13:00:54 -0300

    Updating view form to save images and IPEM data for documents

--- appDocuments/views.py
+++ appDocuments/views.py
@@ -1,12 +1,17 @@
+import io
 import json
-import os
+from pathlib import Path
 
 from django.conf import settings
+
+# import os
+from django.core.files.storage import FileSystemStorage
 from django.http import Http404
 from django.shortcuts import (
     redirect,
     render,  # noqa: F401
 )
+from PIL import Image
 
 # from django.urls import reverse
 from .forms import IpemDataRegisterForm
@@ -17,6 +22,18 @@ def home(request):
 
 
 def ipemData_receive(request):
+    def changeExtensionImage(ImgObj):
+        # Extraindo nomes e extensões dos arquivos
+        ImgObj_aux = Path(ImgObj.name)
+        ImgObj_ext = ImgObj_aux.suffix.lower()
+
+        if ImgObj_ext != '.png':
+            img = Image.open(ImgObj)
+            buffer = io.BytesIO()
+            ImgObj = img.save(buffer, format='PNG')
+
+        return ImgObj
+
     if not request.POST:
         raise Http404()
 
@@ -37,16 +54,40 @@ def ipemData_receive(request):
         }
 
         # Salvando o JSON na pasta do app
-        url_json = settings.BASE_DIR / 'appDocuments'
+        url_json = settings.BASE_DIR / 'appDocuments/ipem-data.json'
 
         # Tentando salvar o JSON
         try:
             with open(url_json, 'w', encoding='UTF-8') as f:
                 json.dump(content, f, indent=4, ensure_ascii=False)
-        except Exception:
-            pass
+        except Exception as e:
+            print('Erro encontrado: ', e)
 
-    # Validação falhou. Renderizando novamente
+        # Obtendo as imagens como objetos em PNG
+        brasao = changeExtensionImage(request.FILES['img_uf'])
+        convenio = changeExtensionImage(request.FILES['img_conv'])
+
+        fs = FileSystemStorage()
+
+        # Lista com nomes das imagens
+        imgs = ['brasao.png', 'convenio.png']
+
+        for img in imgs:
+            # Apagando arquivos pré-existentes
+            if fs.exists(img):
+                fs.delete(img)
+
+        # Salvando imagens na pasta 'media'
+        fs.save('brasao.png', brasao)
+        fs.save('convenio.png', convenio)
+
+        form = IpemDataRegisterForm()
+        return render(
+            request,
+            'appDocuments/pages/ipem_data.html',
+            context={'form': form}
+        )
+
     return redirect('appDocuments:ipem-data-send')
 
 
