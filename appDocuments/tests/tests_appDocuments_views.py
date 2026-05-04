e0e3f8e9c4511f45e6d8362545989b6a78f53890
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-05-04 10:46:34 -0200

    Implementing unit test for high error dispatch Class Based View

--- appDocuments/tests/tests_appDocuments_views.py
+++ appDocuments/tests/tests_appDocuments_views.py
@@ -12,6 +12,16 @@ class appDocumentsViewsTest(SimpleTestCase):
         bcv = views.IpemData
         self.assertIs(bcv, bcv_wanted)
 
+    def test_appDocuments_high_error_dispatch_based_function_view_is_correct(self):
+        resolve_obj = resolve(reverse('appDocuments:high-error-dispatch'))
+        bcv_wanted = resolve_obj.func.view_class
+        bcv = views.HighErrorDispatch
+        self.assertIs(bcv, bcv_wanted)
+
     def test_appDocuments_ipem_data_template_is_correct(self):
         response = self.client.get(reverse('appDocuments:ipem-data-send'))
         self.assertTemplateUsed(response, 'appDocuments/pages/ipem_data.html')
+
+    def test_appDocuments_high_error_dispatch_template_is_correct(self):
+        response = self.client.get(reverse('appDocuments:high-error-dispatch'))
+        self.assertTemplateUsed(response, 'appDocuments/pages/high_error_dispatch.html')
