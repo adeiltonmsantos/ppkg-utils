a9cdc1fabb9cca37ca6ce00cc88b4963e9bdd2ec
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-05-06 08:23:02 -0300

    Refactoring unit tests for views of appDocuments (renaming class and tests)

--- appDocuments/tests/tests_appDocuments_views.py
+++ appDocuments/tests/tests_appDocuments_views.py
@@ -4,24 +4,31 @@ from django.urls import resolve, reverse
 from appDocuments import views
 
 
-class appDocumentsViewsTest(SimpleTestCase):
+class IpemDataViewsTest(SimpleTestCase):
 
-    def test_appDocuments_ipem_data_based_function_view_is_correct(self):
+    def test_ipem_data_based_function_view_is_correct(self):
         resolve_obj = resolve(reverse('appDocuments:ipem-data-send'))
         bcv_wanted = resolve_obj.func.view_class
         bcv = views.IpemData
         self.assertIs(bcv, bcv_wanted)
 
-    def test_appDocuments_high_error_dispatch_based_function_view_is_correct(self):
+    def test_ipem_data_template_is_correct(self):
+        response = self.client.get(reverse('appDocuments:ipem-data-send'))
+        self.assertTemplateUsed(response, 'appDocuments/pages/ipem_data.html')
+
+
+class HighErrorDispatchTest(SimpleTestCase):
+    def test_high_error_dispatch_based_function_view_is_correct(self):
         resolve_obj = resolve(reverse('appDocuments:high-error-dispatch'))
         bcv_wanted = resolve_obj.func.view_class
         bcv = views.HighErrorDispatch
         self.assertIs(bcv, bcv_wanted)
 
-    def test_appDocuments_ipem_data_template_is_correct(self):
-        response = self.client.get(reverse('appDocuments:ipem-data-send'))
-        self.assertTemplateUsed(response, 'appDocuments/pages/ipem_data.html')
-
-    def test_appDocuments_high_error_dispatch_template_is_correct(self):
+    def test_high_error_dispatch_template_is_correct(self):
         response = self.client.get(reverse('appDocuments:high-error-dispatch'))
         self.assertTemplateUsed(response, 'appDocuments/pages/high_error_dispatch.html')
+
+
+class HighErrorDispatchIntegrationTest(SimpleTestCase):
+    def test_high_error_dispatch_form_send_valid_pdf(self):
+        ...
