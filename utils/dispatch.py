8d06e75f4f4315e0d5a40d892f121e1a6e5fcb8a
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-05-11 12:20:06 -0300

    Refactoring dispatch class (starting to translate code to english)

--- utils/dispatch.py
+++ utils/dispatch.py
@@ -42,7 +42,7 @@ uf_choices = {
     'TO': 'Tocantins'
 }
 
-class Despacho(PDF):
+class Dispatch(PDF):
   """
   Classe para gerar o PDF do despacho.
   Argumento obrigatório
@@ -70,43 +70,45 @@ class Despacho(PDF):
                              # vez que header() é chamado em super().__init__()
                              # t_margin é 10, mesmo sendo definido diferente acima
     # Data do despacho
-    hoje = dt.date.today().strftime('%d/%m/%Y')
-    self.data_despacho = args.get('data_despacho', hoje)
+    self.dispatch_date = args.get(
+      'dispatch_date',
+      dt.date.today().strftime('%d/%m/%Y')
+    )
 
     # Percentual da imagem da assinatura. Se não for informado usa 90%
-    self.width_perc_assin = args.get('width_perc_assin', 90)
+    self.width_perc_signature = args.get('width_perc_assin', 90)
 
     # Nome do responsável pelo despacho
-    self.nome_responsavel = args.get('nome_responsavel', '')
+    self.responsable_name = args.get('responsable_name', '')
 
     # Cargo do responsável pelo despacho
-    self.cargo_responsavel = args.get('cargo_responsavel', '')
+    self.responsable_position = args.get('responsable_position', '')
 
     # Tentando carregar url da imagem do brasão do Estado
-    self.url_brasao = None
+    self.url_coat_of_arms = None
     if os.path.exists(settings.MEDIA_ROOT + '/brasao.png'):
-      self.url_brasao = f'{settings.MEDIA_ROOT} /brasao.png'
+      self.url_coat_of_arms = f'{settings.MEDIA_ROOT} /brasao.png'
 
     # Carregando arquivo json com dados do cabeçalho do despacho...
     # Existe arquivo
     try:
       with open(JSON_PATH, 'r') as f:
         ipem_data = json.load(f)
-      self.txt_estado = uf_choices[ipem_data.get('uf_ipem', '')].upper()
-      self.txt_secretaria = ipem_data.get('sec_ipem', '').upper()
+      self.txt_state = uf_choices[ipem_data.get('uf_ipem', '')].upper()
+      self.txt_secretary = ipem_data.get('sec_ipem', '').upper()
       self.txt_ipem = ipem_data.get('rs_ipem', '').upper()
     # Não existe arquivo
     except Exception:
-      self.txt_estado = 'FALTA DEFINIR O NOME DO ESTADO'
-      self.txt_secretaria = 'FALTA DEFINIR O NOME DA SECRETARIA'
+      self.txt_state = 'FALTA DEFINIR O NOME DO ESTADO'
+      self.txt_secretary = 'FALTA DEFINIR O NOME DA SECRETARIA'
       self.txt_ipem = 'FALTA DEFINIR O NOME DO IPEM'
 
     # Tentando carregar a imagem de marca d'água do despacho
     if os.path.exists(AGREEMENT_IMAGE_PATH):
-      self.url_marca_dagua = AGREEMENT_IMAGE_PATH
+      self.watermark = AGREEMENT_IMAGE_PATH
 
     else:
-      self.url_marca_dagua = None
+      self.watermark = None
 
     # O método 'header()' é chamado no __init__() da classe pai. Por isso os
     # itens acima foram definidos antes da chamada de super().__init__(), pois
@@ -115,9 +117,9 @@ class Despacho(PDF):
 
     # Tentando carregar a url da imagem de assinatura (se houver)
     if os.path.exists(IN_CHARGE_SIGNATURE):
-      self.url_assinatura = IN_CHARGE_SIGNATURE
+      self.url_signature = IN_CHARGE_SIGNATURE
     else:
-      self.url_assinatura = None
+      self.url_signature = None
 
     # Lista com erros
     self.list_erros = list_erros
@@ -145,7 +147,7 @@ class Despacho(PDF):
     self.set_font('helvetica', 'B', 12)
     self.cell(16, 5,'DATA: ')
     self.set_font('helvetica', '', 12)
-    self.cell(0, 5, self.data_despacho)
+    self.cell(0, 5, self.dispatch_date)
 
     # Seção 'DE: Divisão de Pré-Medidos'
     self.set_y(self.get_y() + 5)
@@ -175,12 +177,12 @@ class Despacho(PDF):
   # Cria o cabeçalho e define marca dágua (se existir imagem no Drive)
   def header(self):
     self.set_xy(self.l_margin, self.t_margin_cabec)
-    if self.url_brasao is not None:
-      self.renderImage(self.url_brasao, prop_w=10)
+    if self.url_coat_of_arms is not None:
+      self.renderImage(self.url_coat_of_arms, prop_w=10)
 
     self.set_font('Times', 'B', 12)
-    txt_cabec = f'GOVERNO DO ESTADO DE {self.txt_estado}\n'
-    txt_cabec += f'{self.txt_secretaria}\n'
+    txt_cabec = f'GOVERNO DO ESTADO DE {self.txt_state}\n'
+    txt_cabec += f'{self.txt_secretary}\n'
     txt_cabec += f'{self.txt_ipem}\n'
     txt_cabec += 'Órgão Executor do INMETRO'
     self.multi_cell(0, 5, txt_cabec, align='C')
@@ -189,7 +191,7 @@ class Despacho(PDF):
     self.cabec_h = self.get_y()
 
     # Renderizando a marca d'água, se houver imagem salva
-    if self.url_marca_dagua is not None:
+    if self.watermark is not None:
       # A marca d'água deve ocupar 2/3 da altura da página, a seção central
       # Altura da imagem de marca d'água
       marca_h = self.h / 3
@@ -198,7 +200,7 @@ class Despacho(PDF):
       marca_pos_y = marca_h
 
       # Obtendo a relação entre largura e altura da imagem de marca d'água
-      marca = Image.open(self.url_marca_dagua)
+      marca = Image.open(self.watermark)
       w, h = marca.size
       w_h = w / h
       marca.close()
@@ -213,7 +215,7 @@ class Despacho(PDF):
         marca_pos_x = 0
 
       # Renderizando a imagem de marca d'água
-      self.image(self.url_marca_dagua, w=marca_h, h=marca_h, y=marca_pos_y, x=marca_pos_x)
+      self.image(self.watermark, w=marca_h, h=marca_h, y=marca_pos_y, x=marca_pos_x)
 
     self.set_xy(self.l_margin, self.cabec_h + 10)
 
@@ -252,22 +254,22 @@ class Despacho(PDF):
 
     # Se foi informado nome e cargo do responsável, exibe esses valores para
     # assinatura, mesmo que exista imagem de assinatura no Google Drive
-    nome_cargo_definidos = (self.nome_responsavel != '') and (self.cargo_responsavel != '')
+    nome_cargo_definidos = (self.responsable_name != '') and (self.responsable_position != '')
 
     # Foram informados nome e cargo do responsável pelo despacho. Imprime esses
     # valores mesmo que haja imagem de assinatura disponível no Google Drive
     if nome_cargo_definidos:
       self.cell(0, 6, '____________________________________________________', align='C', new_y='NEXT', new_x='LEFT')
-      self.multi_cell(0, 6, f'{self.nome_responsavel}\n{self.cargo_responsavel}', align='C')
+      self.multi_cell(0, 6, f'{self.responsable_name}\n{self.responsable_position}', align='C')
 
     # Nome e cargo do responsável pelo despacho não foram definidos, mas imagem
     # assinatura foi. Imprime a imagem da assinatura
-    elif not nome_cargo_definidos and self.url_assinatura is not None:
-      self.renderImage(self.url_assinatura, prop_w=self.width_perc_assin, y_adic_new_page=self.cabec_h)
+    elif not nome_cargo_definidos and self.url_signature is not None:
+      self.renderImage(self.url_signature, prop_w=self.width_perc_signature, y_adic_new_page=self.cabec_h)
 
     # Não foram definidos nem nome/cargo do responsável nem imagem de assinatura
     # Imprimir 'Nome Responsável' e 'Cargo Responsável'
-    elif not nome_cargo_definidos and self.url_assinatura is None:
+    elif not nome_cargo_definidos and self.url_signature is None:
       self.cell(0, 6, '____________________________________________________', align='C', new_y='NEXT', new_x='LEFT')
       self.multi_cell(0, 6, 'Nome Responsável\nCargo Responsável', align='C')
 
