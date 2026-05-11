16ae8a2ec42269358738602e6c66d526f2b65c1d
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-05-11 12:07:46 -0300

    Refactoring integration test for high error dispatch form (adjusting setup method)

--- appDocuments/tests/tests_high_error_form.py
+++ appDocuments/tests/tests_high_error_form.py
@@ -1,4 +1,5 @@
 import datetime as dt
+from pathlib import Path
 
 from django.test import TestCase
 from django.urls import reverse
@@ -13,9 +14,10 @@ class IntegrationTestHighErrorDispatch(TestCase):
 
     def setUp(self):
         self.form_data = {
-            'dispatch_date': '',
-            'dispatch_pdf': self.make_pdf()
+            'dispatch_date': f'{dt.datetime.today().day}/{dt.datetime.today().month}/{dt.datetime.today().year}',  # noqa:E501
+            'dispatch_pdf': '',
         }
+        self.test_reports_path = Path(__file__).resolve().parent / 'reports_to_test'
         return super().setUp()
 
     def test_high_error_dispatch_validate_date(self):
@@ -27,3 +29,4 @@ class IntegrationTestHighErrorDispatch(TestCase):
             'A data informada é inválida',
             resolve.content.decode('utf-8')
         )
+    
