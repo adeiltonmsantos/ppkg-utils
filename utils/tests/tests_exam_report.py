6c32cab05bf23b5d450acaed943cbcaa417be3de
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-05-13 11:50:10 -0300

    Definig unit test for ExamReport class about return of exam report type correctly

--- utils/tests/tests_exam_report.py
+++ utils/tests/tests_exam_report.py
@@ -2,6 +2,8 @@ import io
 from pathlib import Path
 from unittest import TestCase
 
+from parameterized import parameterized
+
 from utils.exam_report import ExamReport
 
 
@@ -25,24 +27,41 @@ class UnitTestExamReport(TestCase):
             return False
 
 
-    def test_if_pdf_is_a_valid_exam_report(self):
+    def test_if_file_is_a_valid_pdf_exam_report(self):
         # Exam Report Object
         er = self.exam_rep
 
         # Exam report path to test
-        pdf_name = 'ld_mass_02.pdf'
+        pdf_name = 'ld_mass_rp_01.pdf'
 
         # Loading to memory a PDF file to test
         pdf_file = self.loadExamReportPDF(pdf_name)
 
         # Testing if the file is a PDF
-        if pdf_file:
-            # Loading data from PDF file
-            result = er.loadRawData(pdf_file)
+        er.loadRawData(pdf_file)
+        self.assertIn(
+            'SERVIÇO PÚBLICO FEDERAL',
+            er.list_raw_data[0][0],
+            msg="File doesn't exist or is not a PDF file"
+        )
 
-            if result:
-                self.assertIn(
-                    'SERVIÇO PÚBLICO FEDERAL',
-                    er.list_raw_data[0][0]
-                )
+    @parameterized.expand([
+        ('ld_high_rp_01.pdf', 'c'),
+        ('ld_length_rp_01.pdf', 'c'),
+        ('ld_width_rp_01.pdf', 'c'),
+        ('ld_unid_ap_01.pdf', 'u'),
+        ('ld_mass_rp_01.pdf', 'm'),
+        ('ld_vol_rp_01.pdf', 'v'),
+    ])
+    def test_if_returns_type_of_report_exam_correctly(self, filename, type_exam):
+        # Loading to memory a PDF file to test
+        pdf_file = self.loadExamReportPDF(filename)
+
+        self.exam_rep.loadRawData(pdf_file)
+        exam_type = self.exam_rep.getExamType()
+        self.assertIs(exam_type,
+                      type_exam,
+                      msg="File doesn't exist or is invalid"
+        )
+
 
