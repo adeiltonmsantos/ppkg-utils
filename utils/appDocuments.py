a5f7b81e7a749d121eb5e4968880ab51c29397ea
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-05-15 12:31:36 -0300

    Transferring common usage methods from the ExamReport unit test class to utils/appDocuments.py

--- utils/appDocuments.py
+++ utils/appDocuments.py
@@ -1,6 +1,16 @@
+import io
 import os
+from pathlib import Path
 
 from django.conf import settings
+
+from utils.exam_report import (
+    ExamReport,
+    ExamReportLength,
+    ExamReportMass,
+    ExamReportUnit,
+    ExamReportVol,
+)
 
 
 def get_imgs_path():
@@ -21,3 +31,42 @@ def get_imgs_path():
                 imgs_path[img] = None
 
     return imgs_path
+
+def loadExamReportPDF(pdf_name):
+    # Exam report path to test
+    pdf_folder = Path(__file__).parent / 'tests/reports_to_test'
+    pdf_path = pdf_folder / pdf_name
+
+    # Trying to load to memory a PDF file to test
+    try:
+        with open(pdf_path, 'rb') as f:
+            pdf_file = io.BytesIO(f.read())
+            return pdf_file
+    except Exception:
+        return False
+
+def getExamReportObjectByType(filename):
+    # Loading to memory a PDF file to test
+    pdf_file = loadExamReportPDF(filename)
+    er = ExamReport()
+    er.loadRawData(pdf_file)
+    type = er.getExamType()
+    match type:
+        case 'c':
+            er = ExamReportLength()
+            er.loadRawData(pdf_file)
+            er.loadProdData()
+        case 'u':
+            er = ExamReportUnit()
+            er.loadRawData(pdf_file)
+            er.loadProdData()
+        case 'm':
+            er = ExamReportMass()
+            er.loadRawData(pdf_file)
+            er.loadProdData()
+        case 'v':
+            er = ExamReportVol()
+            er.loadRawData(pdf_file)
+            er.loadProdData()
+    if er:
+        return er
