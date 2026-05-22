cc9c5ff883849036da87e5bd7e4f560785c06599
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-05-22 12:48:29 -0300

    Continuing with the code for the post method of the class-based view for high error dispatch

--- appDocuments/views/high_error_dispatch.py
+++ appDocuments/views/high_error_dispatch.py
@@ -1,14 +1,18 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.views import View
 
 from appDocuments.forms.high_error_dispatch import HighErrorDispatchForm
-from utils.exam_report import ExamReport
+from utils.appDocuments import getExamReportObjectByType
+from utils.dispatch import Dispatch
 
+DISPATCH_FOLDER = settings.DISPATCH_PATH or None
 
 class HighErrorDispatch(View):
     def render_template(self, **kwargs):
         form = kwargs.get('form', None)
         form_data = kwargs.get('form_data', None),
+        dispatch_url = kwargs.get('dispatch_url', None)
 
         return render(
             self.request,
@@ -17,6 +21,7 @@ class HighErrorDispatch(View):
                 'form': form,
                 'form_data': form_data,
                 'title_form': 'DESPACHO PARA ERROS ELEVADOS',
+                'dispatch_url': dispatch_url,
             }
         )
     
@@ -26,19 +31,26 @@ class HighErrorDispatch(View):
     
     def post(self, *args, **kwargs):
         data = self.request.POST or None
-        files = self.request.FILES.getlist('dispatch_pdf') or None
+        files = self.request.FILES or None
         form = HighErrorDispatchForm(data=data, files=files)
 
         if form.is_valid():
-            dispatch_date = form.cleaned_data.get('dispatch_date')
-            dispatch_pdf = form.files.get('dispatch_pdf')
+            dispatch_date = form.cleaned_data['dispatch_date'].strftime('%d/%m/%Y')
+            dispatch_pdf = form.files.getlist('dispatch_pdf')
+            errors = []
 
             # Routine to generate dispatch here
-            report = ExamReport()
             for f in dispatch_pdf:
-                rp = report.loadRawData(f)
+                er = getExamReportObjectByType(f)
+                if er.isSubjectToDispatch():
+                    errors.append(er.getErrosTxt())
 
-                pass
+            url = None
+            if len(errors) > 0:
+                dp = Dispatch(errors, dispatch_date=dispatch_date)
+                url = dp.geraDespachoPDF(pathfile=DISPATCH_FOLDER)
+
+            return self.render_template(form=form, dispatch_url=url)
         else:
             return self.render_template(
                 form=form,
