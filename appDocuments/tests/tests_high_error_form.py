947feb2648c88cb612f01b3fd55f472ee91f1f4a
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-05-22 12:49:17 -0300

    Refactoring integration test to load errors from pdf

--- appDocuments/tests/tests_high_error_form.py
+++ appDocuments/tests/tests_high_error_form.py
@@ -1,6 +1,7 @@
 import datetime as dt
-from pathlib import Path
 
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.urls import reverse
 
@@ -17,8 +18,29 @@ class IntegrationTestHighErrorDispatch(TestCase):
             'dispatch_date': f'{dt.datetime.today().day}/{dt.datetime.today().month}/{dt.datetime.today().year}',  # noqa:E501
             'dispatch_pdf': '',
         }
-        self.test_reports_path = Path(__file__).resolve().parent / 'reports_to_test'
+        self.test_reports_path = settings.BASE_DIR / 'utils/tests/reports_to_test'
         return super().setUp()
+
+    def loadExamReportPDF(self, pdf_name):
+        """
+        loadExamReportPDF(pdf_name)
+        Returns a PDF file object in memory based on the name of a PDF in utils/tests/reports_to_test.
+        If file doesn't exist returns False
+        """
+        # Exam report path to test
+        pdf_path = self.test_reports_path / pdf_name
+
+        # Trying to load to memory a PDF file to test
+        try:
+            with open(pdf_path, 'rb') as f:
+                pdf_file = f.read()
+            return SimpleUploadedFile(
+                name=pdf_name,
+                content=pdf_file,
+                content_type='application/pdf'
+            )
+        except Exception:
+            return False
 
     def test_high_error_dispatch_validate_date(self):
         url = reverse('appDocuments:high-error-dispatch')
@@ -29,4 +51,18 @@ class IntegrationTestHighErrorDispatch(TestCase):
             'A data informada é inválida',
             resolve.content.decode('utf-8')
         )
-    
+
+    def test_high_error_dispatch_loads_errors_from_pdf_files(self):
+        self.form_data['dispatch_pdf'] = [
+                    self.loadExamReportPDF('ld_high_rp_01.pdf'),
+                    self.loadExamReportPDF('ld_length_rp_01.pdf'),
+                    self.loadExamReportPDF('ld_mass_rp_01.pdf'),
+        ]
+        
+        resolve = self.client.post(
+            reverse('appDocuments:high-error-dispatch'),
+            data=self.form_data,
+            # format='multpart',
+            follow=True
+        )
+        pass
