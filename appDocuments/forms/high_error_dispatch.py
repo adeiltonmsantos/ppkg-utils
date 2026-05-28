39a4eda26dc4c262df896af53225260ac5a0a0c4
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-05-28 08:34:44 -0300

    Adding analisys for invalid PDFs uploaded in high error dispatch form (clean method)

--- appDocuments/forms/high_error_dispatch.py
+++ appDocuments/forms/high_error_dispatch.py
@@ -5,6 +5,8 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.core.validators import FileExtensionValidator
 
+from utils.exam_report import ExamReport
+
 
 class HighErrorDispatchForm(forms.Form):
     def __init__(self, *args, **kwargs):
@@ -35,7 +37,7 @@ class HighErrorDispatchForm(forms.Form):
         superclean = super().clean()
         obj_dt = self.cleaned_data.get('dispatch_date')
         dispatch_date = f'{obj_dt.day}/{obj_dt.month}/{obj_dt.year}'
-        # dispatch_pdf = self.files.get('dispatch_pdf')
+        dispatch_pdf = self.files.getlist('dispatch_pdf')
 
         # Validating date
         valid_date = False
@@ -50,6 +52,18 @@ class HighErrorDispatchForm(forms.Form):
 
         if not valid_date:
             self.errors_fields['dispatch_date'].append('A data informada é inválida')
+
+        # Validating PDF files
+        list_invalid_pdfs = []
+        for pdf_file in dispatch_pdf:
+            exam_report = ExamReport()
+            valid_file = exam_report.loadRawData(pdf_file)
+            if valid_file is False:
+                list_invalid_pdfs.append(pdf_file.name)
+        if len(list_invalid_pdfs) > 0:
+            msg = f'Arquivo(s) inválido(s) encontrado(s): {', '.join(list_invalid_pdfs)}. '
+            msg += 'Tente novamente sem esse(s) arquivo(s)'
+            self.errors_fields['dispatch_pdf'].append(msg)
 
         if self.errors_fields:
             raise ValidationError(self.errors_fields)
