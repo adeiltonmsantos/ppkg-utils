40d9838ab196e223b583db28d9bfcbfec2ccfea6
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-05-29 08:01:50 -0300

    Activating cancel button from high error dispatch

---
+++ appDocuments/static/appDocuments/js/scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+    const btn_rst = document.querySelector('#btn_reset')
+    btn_rst.addEventListener('click', function(e){
+        e.preventDefault()
+        window.location.href = e.currentTarget.dataset.url
+    })
+
+})
