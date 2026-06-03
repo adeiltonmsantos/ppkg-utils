456012c844e5665f7df296c542f3ec94faad98f9
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-06-03 07:51:46 -0300

    Starting to define extract exam schedule form class

--- appDocuments/forms/__init__.py
+++ appDocuments/forms/__init__.py
@@ -1,2 +1,3 @@
 # flake8: noqa
 from .ipem_data_register import IpemDataRegisterForm
+from .extract_exam_schedule import ExtractExamSchedule
---
+++ appDocuments/forms/extract_exam_schedule.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
+
+from utils.appDocuments import extractScheduleToDictList
+
+
+class ExtractExamSchedule(forms.Form):
+    exam_schedule_pdf = forms.FileField(
+        required=True,
+        label='Cronograma de perícias',
+        validators=[FileExtensionValidator(allowed_extensions=['pdf'])]
+    )
+
+    def clean(self):
+        super().clean()
+        file_schedule = self.files.get('exam_schedule_pdf')
+
+        if file_schedule is not False:
+            return extractScheduleToDictList(file_schedule)
+        else:
+            raise ValidationError('O arquivo enviado não é válido')
+
