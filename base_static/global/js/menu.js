27ddaa51a1019b4f7d122768f4676d5be9c8db56
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-08-25 07:31:54 -0300

    Implementing initial menu (home)

--- base_static/global/js/menu.js
+++ base_static/global/js/menu.js
@@ -9,26 +9,47 @@ document.addEventListener('DOMContentLoaded', function(){
     const menu_items_url = menu_items_container.value
     // User profile (0: common user; 1: superuser)
     const user_profile = menu_items_container.dataset.spr
-    // Original menu items object
-    let menuItemsObj = 'Nada ainda'
 
     // Setting the menu button display
     btn_menu.addEventListener('click', function(){
-
+        menu_container.classList.toggle('visible-collum')
     })
 
+    // Hiding the menu when the mouse is not over it
+    menu_container.addEventListener('mouseleave', function(){
+        menu_container.classList.remove('visible-collum')
+    })
+
+    // Function to build menu items
+    function makeMenuItems(menu){
+        const h1Menu = document.createElement('h1')
+        const ulMenu = document.createElement('ul')
+        h1Menu.innerText = menu.label
+        menu_container.appendChild(h1Menu)
+        menu_container.appendChild(ulMenu)
+        let liMenu = null
+        menu.items.forEach(item => {
+            liMenu = document.createElement('li')
+            liMenu.append(item.label)
+            ulMenu.appendChild(liMenu)
+        });
+    }
 
     // Trying to get menu items
     if (menu_items_url){
-        menuItemsObj = fetch(menu_items_url)
+        fetch(menu_items_url)
             .then(response => {
                 if(!response.ok){
                     throw new Error("Itens de menu não carregados")
                 }
                 return response.json()
             })
+            .then(arrayMenu => {
+                const homeMenu = arrayMenu.find(item => item.id === 'main')
+                makeMenuItems(homeMenu)
+            })
             .catch(error => {
-                console.log("Itens de menu não foram carregados")
+                console.log("JSON carregou mas menu falhou: " + error)
             })
     }
 
