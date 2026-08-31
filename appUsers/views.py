382291261036ddfe107bb6d3c7973af97727e072
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2026-08-31 08:59:51 -0300

    Adjusting flash messages for user register

--- appUsers/views.py
+++ appUsers/views.py
@@ -1,17 +1,22 @@
-from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
 from .forms import UserRegistrationForm
 
 
-class UserRegistrationView(SuccessMessageMixin, CreateView):
+class UserRegistrationView(LoginRequiredMixin, CreateView):
     template_name = 'appUsers/pages/registration_user.html'
     form_class = UserRegistrationForm
     success_url = reverse_lazy('home')
-    success_message = 'Usuário(a) cadastrado(a) com sucesso!'
 
     def get_context_data(self, *args, **kwargs):
         cnt = super().get_context_data(**kwargs)
         cnt['title_page'] = 'Cadastro de Usuário'
         return cnt
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Usuário(a) cadastrado(a) com sucesso!')
+        return super().form_valid(form)
+    
